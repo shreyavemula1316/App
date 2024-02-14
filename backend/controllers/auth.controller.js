7663c38ad9d9f60ac37590e184a3a96da0f90424
shreyavemula1316 <shreyavemula16@gmail.com>
2024-02-14 17:09:53 +0530

    updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
+import bcrypt from 'bcryptjs';
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from '../utils/generateToken.js';
 
 export const signup = async (req,res) =>{
    try {
@@ -7,12 +9,15 @@ export const signup = async (req,res) =>{
             return res.status(400).json({error:"Passwords don't match"});
         }
         const user = await User.findOne({username});
-
+        
         if(user){
             return res.status(400).json({error:"Username already exists"});
         }
-
+       
         //HASH PASSWORD HERE
+
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password,salt);
 
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=Scott=${username}`;
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=Maria=${username}`;
@@ -20,19 +25,30 @@ export const signup = async (req,res) =>{
         const newUser = new User({
             fullName,
             username,
-            password,
+            password:hashPassword,
             gender,
-            ProfilePic: gender === "male" ? boyProfilePic:girlProfilePic
+            profilepic: gender === "male" ? boyProfilePic : girlProfilePic
         });
+    
+        
+        if(newUser){
 
-        await newUser.save();
+            //Generate JWT token
+            generateTokenAndSetCookie(newUser._id,res);
 
-        res.status(201).json({
-            __id: newUser._id,
-            fullName:newUser.fullName,
-            username:newUser.username,
-            ProfilePic:newUser.ProfilePic
-        })
+            await newUser.save();
+
+            res.status(201).json({
+                __id: newUser._id,
+                fullName:newUser.fullName,
+                username:newUser.username,
+                profilepic:newUser.profilepic
+            })
+        }
+
+        else{
+            res.status(400).json({error:"Invalid User data"});
+        }
 
    } catch (error) {
         console.log("Error in signup controller", error.message);
@@ -40,11 +56,40 @@ export const signup = async (req,res) =>{
    }
 };
 
-export const login = (req,res) =>{
-    console.log("loginuser");
+export const login = async (req,res) =>{
+   try {
+
+    const {username,password} = req.body;
+    const user = await User.findOne({username});
+    const isPasswordCorrect = await bcrypt.compare(password,user?.password || "");
+
+    if(!user || !isPasswordCorrect){
+        return res.status(400).json({error: "Invalid Username or password"});
+    }
+
+    generateTokenAndSetCookie(user._id,res);
+
+    res.status(200).json({
+        __id: user._id,
+        fullName: user.fullName,
+        username: user.username,
+        profilepic:user.profilepic,
+    });
+    
+   } catch (error) {
+    console.log("Error in Login controller", error.message);
+    res.status(500).json({error:"Internal Server Error"});
+   }
 };
 
 export const logout = (req,res) =>{
-    console.log("logout");
+   try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
+    
+   } catch (error) {
+    console.log("Error in Logout controller", error.message);
+    res.status(500).json({error:"Internal Server Error"});
+   }
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
 import connectTOMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
 
 
 const app = express();
@@ -9,8 +11,10 @@ const PORT = 5000;
 
 dotenv.config();
 app.use(express.json()); //to parse the incoming requests with Json payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth/",authRoutes);
+app.use("/api/messages",messageRoutes);
 
 app.listen(PORT ,() =>{
     connectTOMongoDB();
